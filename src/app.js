266d23e51e5f0ca01ead70e0efbf2167f016280e
IvayloAsenov <ivaylo.asenov@hotmail.com>
2018-12-18 21:38:40 -0500

    work with exports

--- src/app.js
+++ src/app.js
@@ -1,2 +1,8 @@
-console.log('app.js');
+// import subtract, {square, add} from "./utils.js";
 
+// console.log('app.js is running');
+// console.log(square(5), add(5,5), subtract(5, 5));
+
+import isSenior, { isAdult, canDrink } from "./person.js";
+
+console.log(isAdult(50), canDrink(18), isSenior(65));
